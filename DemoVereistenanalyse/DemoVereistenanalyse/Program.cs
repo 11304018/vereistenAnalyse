ad5b810d1b38fb225de1dd95c12e82a65ae0ecfe
Roel Knapen <roel.knapen@student.pxl.be>
2025-11-06 13:34:07 +0100

    DEMO-01

---
+++ DemoVereistenanalyse/DemoVereistenanalyse/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoVereistenanalyse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef de gebruikersnaam in: ");
+            string inputGebruikersnaam = Console.ReadLine();
+        }
+    }
+}
