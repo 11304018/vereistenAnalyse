f9887e037b81403f3d1e05fd1e77dc41ea2d6204
Roel Knapen <roel.knapen@student.pxl.be>
2025-11-06 13:40:56 +0100

    DEMO-03

--- DemoVereistenanalyse/DemoVereistenanalyse/Program.cs
+++ DemoVereistenanalyse/DemoVereistenanalyse/Program.cs
@@ -8,6 +8,15 @@
             string inputGebruikersnaam = Console.ReadLine();
             Console.Write("Geef het wachtwoord in: ");
             string inputWachtwoord = Console.ReadLine();
+
+            string gebruikersnaam = "Admin";
+            string wachtwoord = "Passwoord123";
+
+            if (inputGebruikersnaam == gebruikersnaam && inputWachtwoord == wachtwoord)
+            {
+                Console.WriteLine($"Welkom {inputGebruikersnaam}!");
+                return;
+            }
         }
     }
 }
